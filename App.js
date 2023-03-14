90adb7393782c30ecf62d910c6a21992a9537a68
shubham kumar <devshubhamyadav@gmail.com>
2023-03-14 13:29:05 +0530

    setup

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-import React from "react";
-import "react-native-gesture-handler";
-import "./src/utils/firebase";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <RootNavigation />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
