726a3382faa42ad22fa3ba3380e262a0206b992c
shubham kumar <devshubhamyadav@gmail.com>
2023-03-15 17:04:31 +0530

    firebase auth

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDD4aBR1DAHdpCMN7w8ahpwCj-M8wS6qwg",
-  authDomain: "usocial19.firebaseapp.com",
-  projectId: "usocial19",
-  databaseURL: "https://usocial19-default-rtdb.firebaseio.com",
-  storageBucket: "usocial19.appspot.com",
-  messagingSenderId: "241581204168",
-  appId: "1:241581204168:web:a114b413f5103f7a78fbfd",
-  measurementId: "G-P4Z57L5Q3T",
+  apiKey: "*********************",
+  authDomain: "projectid.firebaseapp.com",
+  projectId: "projectid",
+  databaseURL: "https://projectid.firebaseio.com",
+  storageBucket: "projectid.appspot.com",
+  messagingSenderId: "*********************",
+  appId: "*********************",
+  measurementId: "********************",
 };
 
 // Initialize Firebase
